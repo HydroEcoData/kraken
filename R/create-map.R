8232f72221aaa06cd2cae986d36a3a220ab53cb0
Challinger <susanna.challinger@sepa.org.uk>
2025-06-10 16:08:23 +0100

    add: improve map

--- R/create-map.R
+++ R/create-map.R
@@ -3,19 +3,25 @@ create_map <- function(data, areas, method) {
   # Convert survey data to spatial
   test <- sf::st_as_sf(data$survey_data,
     coords = c("Longitude", "Latitude"),
-    crs = 4326
+    crs = "+proj=lonlat"
+    #crs = 4326
   )
+
+  test_merc <- sf::st_transform(test,3857)
+  coastlinemap <- maptiles::get_tiles(
+    test_merc,provider = "CartoDB.PositronNoLabels", zoom = 14)
+
   # Calculate area without overrides
   ellipse <- areas$ellipse
   polygon <- areas$polygon
-  browser()
+  #browser()
   if (method == "iqi") {
     my_colors <- data.frame(colour = c(
-      "#d7191c",
-      "#fdae61",
-      "#fecc5c",
-      "#abdda4",
-      "#2b83ba"
+      "#d8181c",
+      "#fe8c01",
+      "#f5cc0a",
+      "#a5d22d",
+      "#4682b8"
     ), status = c(
       "Bad",
       "Poor",
@@ -44,7 +50,7 @@ create_map <- function(data, areas, method) {
   )
   blue_theme <- ggplot2::theme(
     panel.background = ggplot2::element_rect(
-      fill = "#BFD5E3", colour = "#6D9EC1",
+      fill = "#CBDAED", colour = "#6D9EC1",
       size = 2, linetype = "solid"
     ),
     panel.grid.major = ggplot2::element_line(
@@ -59,15 +65,19 @@ create_map <- function(data, areas, method) {
 
   # names(my_colours$colour) <- levels(data$`WFD status`)
   colScale <- ggplot2::scale_colour_manual(
-    name = "Status",
+    name = "WFD Status",
     values = my_colours$colour
   )
+
+
   g <- ggplot2::ggplot() +
+    tidyterra::geom_spatraster_rgb(data = coastlinemap) +
     ggplot2::geom_sf(data = test, ggplot2::aes(color = .data$`WFD status`)) +
     ggplot2::geom_sf(data = ellipse, alpha = 0) +
     # geom_sf(data = polygon, alpha = 0, colour = "purple") +
     # geom_sf(data = points, colour = "black") +
     colScale +
-    blue_theme
+    blue_theme +
+    ggspatial::annotation_scale(location = "bl", width_hint = 0.2, style = "ticks")
   return(g)
 }
